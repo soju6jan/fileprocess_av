5e4e10f80498609beb14fcd54d7778e1c28220ba
soju6jan <cybersol@naver.com>
2020-06-24 17:29:08 +0900

    1

--- logic_normal.py
+++ logic_normal.py
@@ -37,6 +37,14 @@ class LogicNormal(object):
     @celery.task
     def task():
         try:
+            #2020-06-24
+            # celery 에서는 proxy 항상 None
+            try:
+                from av_agent.logic_normal import LogicNormal
+                LogicNormal.proxy_init()
+            except:
+                pass
+            
             logger.debug('%s INIT SCHEDULER', __name__)
             
             job_list = []
